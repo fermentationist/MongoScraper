e8c772bed69ce937491aabfec6dda1249c3c6c20
Dennis Hodges <fermentationist@gmail.com>
2019-04-15 04:35:23 -0500

    removed nodemon from start script for production

--- scraper.js
+++ scraper.js
@@ -18,15 +18,9 @@ const Scraper = (function (){
 		let articleArray = [];
 		$("main article").each(function (i, articleElement){
 			const article = {};
-<<<<<<< HEAD
 			article.title = $(articleElement).find("h1, h2, h3, h4").text().trim() || null;
 			let link = $(articleElement).find("a").attr("href") || "Link missing";
 			article.link = link.slice(0,4) === "http" ? link : url + link;
-=======
-			console.log("articleElement = ", articleElement);
-			article.title = $(articleElement).find("a").text().trim() || "Untitled";
-			article.link = $(articleElement).find("a").attr("href") || "Link missing";
->>>>>>> 8c17548792d287956b365e5c5694c033dda0b311
 			article.photoURL = $(articleElement).find("img").attr("src") || null;
 			article.summary = $(articleElement).find("p.summary").html() || null;
 			if(article.summary){
